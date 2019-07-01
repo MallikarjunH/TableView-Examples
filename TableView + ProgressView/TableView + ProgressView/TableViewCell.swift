97a5a6c875813f36e8da454d156a21fbeda6957f
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-07-01 14:57:20 +0530

    Added UI and VC

---
+++ TableView + ProgressView/TableView + ProgressView/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TableView + ProgressView
+//
+//  Created by mallikarjun on 01/07/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TableView + ProgressView/TableView + ProgressView/ViewController.swift
+++ TableView + ProgressView/TableView + ProgressView/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    @IBOutlet weak var progressViewOutlet: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let transform : CGAffineTransform = CGAffineTransform(scaleX: 1.0, y: 3.0)
+        progressViewOutlet.transform = transform
+        progressViewOutlet.layer.cornerRadius = 4
+        progressViewOutlet.clipsToBounds = true
     }
 
+    func updateProgressBar(){
+        
+       // let percentageInFloat = CGFloat(percentageValue)
+        progressViewOutlet.progress = Float(0.01 * 20)
+    }
+    
 
 }
 
