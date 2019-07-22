c683562480f9b53e18db8f7b93e04d26c5a6bfd9
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-07-22 13:15:34 +0530

    updte

---
+++ TableViewExpand + File Picker/TableViewExpand + File Picker/AllTableViewCells.swift
@@ -0,0 +1,78 @@
+//
+//  AllTableViewCells.swift
+//  TableViewExpand + File Picker
+//
+//  Created by mallikarjun on 18/07/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import UIKit
+
+class TablewViewCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+
+class TablewViewCell2: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
+class TablewViewCell3: UITableViewCell {
+    
+    
+    @IBOutlet weak var uploadDocumentButton: UIButton!
+    @IBOutlet weak var uploadDocumentImg1: UIImageView!
+    @IBOutlet weak var uploadDocumentImg2: UIImageView!
+    @IBOutlet weak var uploadedFileNameLabel: UILabel!
+    @IBOutlet weak var removeSelectedFileButton: UIImageView!
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
+class TablewViewCell4: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
