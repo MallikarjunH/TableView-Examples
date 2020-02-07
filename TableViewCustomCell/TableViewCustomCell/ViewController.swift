e67495c10eac51ff300a05dc6aeda5e91c96c51e
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2020-02-07 16:56:41 +0530

    CustomCell Example

---
+++ TableViewCustomCell/TableViewCustomCell/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TableViewCustomCell
+//
+//  Created by ADDA on 07/02/20.
+//  Copyright © 2020 ADDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
