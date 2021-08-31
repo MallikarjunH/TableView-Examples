05e1590c4e0ca4b8295b55ab9f6f25337b14451c
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2021-08-31 14:41:14 +0530

    TableView Row Rows move/drag/render example added

---
+++ Table_reorderable_More-Rows/Makefile
@@ -0,0 +1,2 @@
+format:
+	swiftformat --indentcase true --stripunusedargs unnamed-only --self insert --disable spaceAroundOperators,blankLinesAtStartOfScope,blankLinesAtEndOfScope .
---
+++ Table_reorderable_More-Rows/NewspaperExample/StoriesTableViewController.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+struct Headline {
+
+    var id : Int
+    var title : String
+    var text : String
+    var image : String
+
+}
+
+class StoriesTableViewController: UITableViewController {
+
+    var headlines = [
+        Headline(id: 1, title: "Lorem Ipsum", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id ornare tortor, quis dictum enim. Morbi convallis tincidunt quam eget bibendum. Suspendisse malesuada maximus ante, at molestie massa fringilla id.", image: "Apple"),
+        Headline(id: 2, title: "Aenean condimentum", text: "Ut eget massa erat. Morbi mauris diam, vulputate at luctus non, finibus et diam. Morbi et felis a lacus pharetra blandit.", image: "Banana"),
+        Headline(id: 3, title: "In ac ante sapien", text: "Aliquam egestas ultricies dapibus. Nam molestie nunc in ipsum vehicula accumsan quis sit amet quam. Sed vel feugiat eros.", image: "Cantaloupe"),
+        ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.isEditing = true
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return headlines.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+
+        let headline = headlines[indexPath.row]
+        cell.textLabel?.text = headline.title
+        cell.detailTextLabel?.text = headline.text
+        cell.imageView?.image = UIImage(named: headline.image)
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedObject = self.headlines[sourceIndexPath.row]
+        headlines.remove(at: sourceIndexPath.row)
+        headlines.insert(movedObject, at: destinationIndexPath.row)
+        debugPrint("\(sourceIndexPath.row) => \(destinationIndexPath.row)")
+        // To check for correctness enable: self.tableView.reloadData()
+    }
+}
